cc222c782ba28a750925e7e41030a6e1de9ee1cd
Renato Lomba <rntlomba@gmail.com>
2022-07-11 22:00:40 -0300

    Páginas e rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter as RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Router } from './router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello world</h1>
+      <RouterProvider>
+        <Router />
+      </RouterProvider>
 
       <GlobalStyle />
     </ThemeProvider>
