c90744cfb664ed015054949882391a78f1a06dc5
Renato Lomba <rntlomba@gmail.com>
2022-07-12 20:37:37 -0300

    Páginas e rotas - Aprimorando os inputs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,13 +16,28 @@ export function Home() {
         <TaskFormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
             type="text"
             id="task"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
           <label htmlFor="minuteAmount">durante</label>
-          <MinuteAmountInput placeholder="00" type="number" id="minuteAmount" />
+          <MinuteAmountInput
+            placeholder="00"
+            type="number"
+            id="minuteAmount"
+            step={5}
+            min={0}
+            max={60}
+          />
 
           <span>minutos.</span>
         </TaskFormContainer>
